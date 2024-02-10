dd1fcde4c5e2887c95c4b631ac69273140ecbc0d
erdoansujon <erdoan.sujon@gmail.com>
2024-02-10 18:45:41 +0600

    added-portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,340 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  github,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Wordpress Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "HTML-5",
+    company_name: "Fiverr",
+    icon: html,
+    iconBg: "#383E56",
+    date: "March 2021 - 2024",
+    points: [
+      "Semantics: Clearer structure with semantic tags.",
+      "Multimedia: Native support for audio and video.",
+      "Canvas: Dynamic graphics with the <canvas> element.",
+      "Offline Web: Application Cache for offline access.",
+      "Responsive Design: Media queries for adaptable layouts.",
+    ],
+  },
+  {
+    title: "CSS 3",
+    company_name: "Fiverr",
+    icon: css,
+    iconBg: "#383E56",
+    date: "March 2022 - 2024",
+    points: [
+      "Layout Improvements: CSS3 introduces Flexbox and Grid Layout for efficient page structuring.",
+      "Transitions and Animations: Native support for smooth transitions and animations without JavaScript.",
+      "Responsive Design: Media queries enable styling based on device characteristics, enhancing responsiveness.",
+      "Advanced Selectors: CSS3 offers advanced selectors for more precise element styling.",
+      "Customization: CSS3 allows custom fonts with @font-face and introduces variables for flexible stylesheets.",
+    ],
+  },
+  {
+    title: "Tailwind CSS",
+    company_name: "Fiverr",
+    icon: tailwind,
+    iconBg: "#383E56",
+    date: "March 2021 -  2024",
+    points: [
+      "Utility-First Approach: Tailwind CSS uses pre-defined utility classes for direct HTML styling.",
+      "Modular and Configurable: Highly modular and configurable to adapt to project needs.",
+      "Responsive Design: Simplifies responsive design with built-in classes for different screen sizes.",
+      "Just-in-Time Compilation: Optimizes CSS for production with Just-in-Time compilation.",
+      "Extensible Plugins: Supports plugins for easy extension and customization.",
+    ],
+  },
+  {
+    title: "JavaScript",
+    company_name: "Fiverr",
+    icon: javascript,
+    iconBg: "#383E56",
+    date: "March 2021 - 2024",
+    points: [
+      "Client-Side Scripting: Enhances browser interactivity.",
+      "Versatile Paradigms: Supports both OOP and functional programming.",
+      "Event-Driven: Responds to user actions for dynamic applications.",
+      "Asynchronous: Handles tasks without blocking other code.",
+      "Cross-Browser Compatibility: Works on all major browsers for widespread use.",
+    ],
+  },
+  
+  {
+    title: "React Developer",
+    company_name: "Fiverr",
+    icon: reactjs,
+    iconBg: "#E6DEDD",
+    date: "June 2022 - 2024",
+    points: [
+      "Component-Based: Structures UI as reusable components for easy development.",
+      "Virtual DOM: Enhances performance by minimizing direct DOM manipulation.",
+      "Declarative Syntax: Simplifies code readability and debugging.",
+      "One-Way Data Binding: Ensures predictable data flow for better state management.",
+      "React Native: Enables mobile app development for iOS and Android using React concepts.",
+    ],
+  },
+  {
+    title: "Node JS",
+    company_name: "Fiverr",
+    icon: nodejs,
+    iconBg: "#383E56",
+    date: "March 2022 - 2024",
+    points: [
+      "JavaScript Runtime: Node.js enables server-side JavaScript execution using the V8 engine.",
+      "Event-Driven and Non-blocking I/O: Its event-driven architecture allows handling multiple connections concurrently with non-blocking I/O operations.",
+      "NPM (Node Package Manager): NPM is the default package manager, providing a vast ecosystem of reusable modules.",
+      "Scalability and Real-time Applications: Node.js is well-suited for scalable network applications and real-time functionalities.",
+      "Cross-Platform and Open Source: It's cross-platform (Windows, macOS, Linux) and open source with an active community.",
+    ],
+  },
+  {
+    title: "Mongo DB",
+    company_name: "Shopify",
+    icon: mongodb,
+    iconBg: "#383E56",
+    date: "Jan 2022 - 2024",
+    points: [
+      "NoSQL Database: MongoDB is a flexible, NoSQL document store.",
+      "Scalable and Fast: Designed for scalability with high-performance capabilities.",
+      "Dynamic Schema: Allows dynamic and evolving data structures.",
+      "Query Language: Rich query support with indexing and aggregation.",
+      "Geospatial Capabilities: Built-in support for geospatial data and queries.",
+    ],
+  },
+  {
+    title: "Git",
+    company_name: "Fiverr",
+    icon: git,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "VCS: Git is a version control system for tracking code changes.",
+      "Branching: Supports efficient branching and merging workflows.",
+      "Commits: Tracks changes with detailed commit history.",
+      "Distributed: Operates as a distributed system, enabling offline work.",
+      "GitHub: Frequently used with GitHub for remote collaboration.",
+    ],
+  },
+  {
+    title: "Git Hub",
+    company_name: "Meta",
+    icon: github,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
